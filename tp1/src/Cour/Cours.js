1658d528111c57f9fe8924dbddcf44e68db81347
lng105 <lng105@outlook.com>
2023-03-12 21:59:57 -0400

    Page cours liste filter

--- tp1/src/Cour/Cours.js
+++ tp1/src/Cour/Cours.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ListeCours from "./ListeCours";
 import NewCours from "./NewCours"
+import "./Cours.css"
 
 function Cours(){
     const [cours, setCours] = useState([
@@ -10,7 +11,7 @@ function Cours(){
             discipline:"Informatique",
             etudiants: "60",
             debut: "01/01/23",
-            fin: "06/06/23"
+            fin: "06/06/23",
         },
         {
             id: "c2",
@@ -18,15 +19,15 @@ function Cours(){
             discipline:"Informatique",
             etudiants: "90",
             debut: "01/01/23",
-            fin: "06/06/23"
+            fin: "06/06/23",
         },
         {
             id: "c3",
             titre:"Arithmétique et logique",
             discipline:"Informatique",
             etudiants: "80",
-            debut: "1/01/23",
-            fin: "06/06/23"
+            debut: "01/01/23",
+            fin: "06/06/23",
         },
         {
             id: "c4",
@@ -34,7 +35,31 @@ function Cours(){
             discipline:"Informatique",
             etudiants: "50",
             debut: "01/01/23",
-            fin: "06/06/23"
+            fin: "06/06/23",
+        },
+        {
+            id: "c5",
+            titre:"Programmation structurée",
+            discipline:"Informatique",
+            etudiants: "90",
+            debut: "01/01/22",
+            fin: "06/06/22",
+        },
+        {
+            id: "c6",
+            titre:"Programmation structurée",
+            discipline:"Informatique",
+            etudiants: "90",
+            debut: "01/01/21",
+            fin: "06/06/21",
+        },
+        {
+            id: "c7",
+            titre:"Programmation structurée",
+            discipline:"Informatique",
+            etudiants: "90",
+            debut: "07/01/20",
+            fin: "15/12/20",
         },
     ]);
 
@@ -42,9 +67,28 @@ function Cours(){
         setCours(cours.concat(newCours));
     }
 
+    
+    const filterByCours = (debut)=>{
+        setCours(cours.filter(cour=>{
+            return cour.debut === debut;
+        }))
+    }
+
+    const debuts = Array.from(
+        new Set(cours.map((cour) => cour.debut))
+    );
+
     return(
        <div>
             <NewCours addresseMethode={ajouterCours}/>
+            <select onChange={e=>filterByCours(e.target.value)} className="center-text">
+                <option value="" disabled selected>
+                    Select session
+                </option>
+                {debuts.map(debut =>{
+                    return <option key={debut}>{debut}</option>
+                })}
+            </select>
             <ListeCours cours={cours}/>
        </div>
     );
